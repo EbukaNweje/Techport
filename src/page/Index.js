00d1f6cc0db6cd730c86c3b5b342c82422e67248
ekenecf <ekens2u@gmail.com>
2022-12-29 19:27:54 +0100

    resolved merge conflict

--- src/page/Index.js
+++ src/page/Index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 // import Home from "../components/Home"
 import About from '../components/About'
+import Services from '../components/Services'
 import "../css/style.css"
 import "../css/mobie.css"
 
 const index = () => {
   return (
     <div>
-        <About/>
+      <Home/>
+      <About/>
+      <Services />
     </div>
   )
 }
