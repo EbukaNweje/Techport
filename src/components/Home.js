d01602a818b5afd4ba91f1c96747d1284fe96a36
EbukaNweje <nwejeebuka@gmail.com>
2022-12-29 15:43:37 +0100

    Set up

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='Container'>home</div>
+  )
+}
+
+export default home
---
+++ src/page/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from "../components/Home"
+import "../css/style.css"
+import "../css/mobie.css"
+
+const index = () => {
+  return (
+    <div>
+        <Home/>
+    </div>
+  )
+}
+
+export default index
